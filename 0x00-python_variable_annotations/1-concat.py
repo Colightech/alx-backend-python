0bcd20571bed9b6eeb254489c9801ba49b2958c6
Colightech <saintchris4@gmail.com>
2024-05-02 17:49:10 +0000

    task 1-concat.py completed

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,6 +3,7 @@
 string str1 and a string str2 as arguments and
 returns a concatenated string"""
 
+
 def concat(str1: str, str2: str) -> str:
     """this function  returns a concatenated string"""
     return str1 + str2
