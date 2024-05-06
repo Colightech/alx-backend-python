06e052e8c98af96e0d3404b449348c008f6cfb15
Colightech <saintchris4@gmail.com>
2024-05-06 14:46:10 +0000

    task 2-measure_runtime.py completed

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Module that measures elapsed time from async functions"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Function that measures appropriate elapsed time"""
+
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end - start
+    return total_time / n
